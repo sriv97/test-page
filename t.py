3b46cb8d294dd3779f9641937d2c23566e5629d2
Sri Velagapudi <srivar1997@gmail.com>
2019-02-05 10:07:42 -0500

    initial commit

---
+++ t.py
@@ -0,0 +1,14 @@
+grade = int(input('Enter your exam score: '))
+
+if grade >= 85:
+    print('You received an A!')
+elif(grade >=79):
+    print('You received a B!')
+elif(grade >=69):
+    print('You received a C!')
+elif(grade >=60):
+    print('You received a D!')
+else:
+    print('You Failed')
+
+print('Good bye')
